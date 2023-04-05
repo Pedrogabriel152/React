149de5ee1e72a0e1ddb8395b6d7d89d8cb4d4392
Pedro Gabriel <pedrpg5@gmail.com>
2023-04-05 15:59:08 -0300

    Atualizando

---
+++ chamados/src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const SignIn = () =>{
+  return(
+    <div>
+      <h1>Pagina login</h1>
+    </div>
+  )
+}
+
+export default  SignIn;
---
+++ chamados/src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () =>{
+  return(
+    <div>
+      <h1>Pagina Cadastro !</h1>
+    </div>
+  )
+}
+
+export default  SignUp;
