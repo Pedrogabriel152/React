d0a2c140b52e038db16ca3676d243e33a74faa81
Pedro Gabriel <pedrpg5@gmail.com>
2023-06-01 09:54:15 -0300

    Progredindo em styled components

--- styled-components/src/Components/Container/styled.js
+++ styled-components/src/Components/Container/styled.js
@@ -11,4 +11,8 @@ export const Content = styled.section`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+
+    @media (max-width: 800px) {
+        flex-direction: column;
+    }
 `;
